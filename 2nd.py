91d1eb1e4097ec00a1ad820079fc16f1a63c585a
Md Masud Rana <118968969+Termuxmasud404@users.noreply.github.com>
2023-03-18 12:10:02 +0600

    Create 2nd.py

---
+++ 2nd.py
@@ -0,0 +1,29 @@
+import os
+
+import time
+
+import sys
+
+# Set color
+
+G = '\033[1;32m' # Green
+
+def delay_print(s):
+
+    for c in s:
+
+        sys.stdout.write(c)
+
+        sys.stdout.flush()
+
+        time.sleep(0.003)
+
+delay_print
+
+delay_print (""+G+"╔════════════════════════════════════════╗  ""\n")
+
+delay_print (""+G+"║ Instagram: @ 0hacker_x0                ║  ""\n")
+
+delay_print (""+G+"║ Github: https://github.com/MrHacker-X/ ║  ""\n")
+
+delay_print (""+G+"╚════════════════════════════════════════╝  ""\n")
